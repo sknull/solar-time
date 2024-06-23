a0f7fed8799c2b73bf535dfe55e6dd5de99a0407
Stephan Knull <s.knull@t-online.de>
2024-06-23 15:31:57 +0200

    Add scratch test

---
+++ src/test/kotlin/de/visualdigits/solartime/SolarTimeTest.kt
@@ -0,0 +1,27 @@
+package de.visualdigits.solartime
+
+import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
+import java.time.OffsetDateTime
+import java.time.ZonedDateTime
+
+class SolarTimeTest {
+
+    private val now = OffsetDateTime.now().toZonedDateTime()// .of(2024, 12,24, 12, 0, 0,0, ZoneId.systemDefault())
+    // Coordinates of Hamburg, Germany
+    private val lat = 53.551
+    private val lon = 9.994
+
+    @Test
+    fun test() {
+        println("Astronomical dawn: ${SolarTime.calculateAstronomicalDawn(now, lat, lon)}")
+        println("Nautical dawn    : ${SolarTime.calculateNauticalDawn(now, lat, lon)}")
+        println("Civil dawn       : ${SolarTime.calculateCivilDawn(now, lat, lon)}")
+        println("Sunrise          : ${SolarTime.calculateSunrise(now, lat, lon)}")
+        println("Solar noon       : ${SolarTime.calculateSolarNoon(now, lat, lon)}")
+        println("Sunset           : ${SolarTime.calculateSunset(now, lat, lon)}")
+        println("Civil dusk       : ${SolarTime.calculateCivilDusk(now, lat, lon)}")
+        println("Nautical dusk    : ${SolarTime.calculateNauticalDusk(now, lat, lon)}")
+        println("Astronomical dusk: ${SolarTime.calculateAstronomicalDusk(now, lat, lon)}")
+    }
+}
